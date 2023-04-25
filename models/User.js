48631b9b1ce4f4d12957c9f9667cd33516bea1cc
hannazo <106128732+hannazo@users.noreply.github.com>
2023-04-25 11:36:27 -0400

    added Thought model and revised User model

--- models/User.js
+++ models/User.js
@@ -1,51 +1,51 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
 // Schema to create User model
 const userSchema = new Schema(
-  {
-    username: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill a valid email address']
-    },
-    thoughts: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Thought',
-      },
-    ],
-    friends: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: 'User',
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
         },
-      ],
-  },
-  {
-    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
-    toJSON: {
-      virtuals: true,
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
     },
-    id: false,
-  }
+    {
+        // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
 );
 
 // Create a virtual property `friendCount` that retrieves the length of the user's friends array field
 userSchema
-  .virtual('friendCount')
-  // Getter
-  .get(function () {
-    return this.friends.length;
-  });
+    .virtual('friendCount')
+    // Getter
+    .get(function () {
+        return this.friends.length;
+    });
 
-// Initialize our User model
+// Initialize User model
 const User = model('User', userSchema);
 
 module.exports = User;
